cd20c456cd8912e7984834fd3954bec1f26b7a87
Jeremy Woertink <jeremywoertink@gmail.com>
2014-06-23 08:31:27 -0600

    spec helper config updated

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,12 @@
 require 'rubygems'
 require 'bundler/setup'
 
+$: << File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'html_aide'
+
 RSpec.configure do |config|
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.order = "random"
+  config.color = true
 end
