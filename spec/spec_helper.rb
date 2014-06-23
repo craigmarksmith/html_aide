1178c0fdaa8fec9023e95c59d5ed5ecfeeca9553
Jeremy Woertink <jeremywoertink@gmail.com>
2014-06-23 08:17:58 -0600

    setup for rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'bundler/setup'
+
+RSpec.configure do |config|
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.order = "random"
+end
